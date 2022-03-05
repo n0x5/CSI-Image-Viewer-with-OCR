3f19f93bb9cb0cc6f1f7f39608b4a7b90f51a1e3
n0x5 <n0x5@users.noreply.github.com>
2022-03-05 20:33:12 +0100

    screenshot ocr

--- CSI_Image_Rec.py
+++ CSI_Image_Rec.py
@@ -4,6 +4,7 @@ import pytesseract
 from tkinter import *
 import os
 import argparse
+from mss import mss
 
 parser = argparse.ArgumentParser()
 parser.add_argument('filename')
@@ -57,12 +58,35 @@ def save_dir():
                         img_file2.write(img_text3)
             except Exception:
                 pass
+            
+# the screenshot works but doesn't load in view atm for some reason
+def screenshot(text1, text2):
+    window.iconify()
+    with mss() as sct:
+        screen = sct.shot()
+        screen2 = sct.shot()
+        image_load_screen = PIL.Image.open(screen)
+        img_text_screen = pytesseract.image_to_string(image_load_screen)
+        text1.forget()
+        image_load_resize_screen = PIL.Image.open(screen2)
+        img_screen = PIL.ImageTk.PhotoImage(image_load_resize_screen)
+        text1 = Label(image = img_screen, width=400)
+        text1.pack(side=LEFT)
+        text2.forget()
+        text2 = Text( width=300)
+        text2.pack(side=RIGHT)
+        text2.insert('1.0', img_text_screen)
+        scrollbar = Scrollbar(text2, orient='vertical', command=text2.yview)
+        text2['yscrollcommand'] = scrollbar.set
+        scrollbar.place(x=500, y=10, height=500)
+    window.deiconify()
 
 button_single = Button(window, text="Save text to .csi file", command=save_single,  width = '30', height = '4')
 button_all = Button(window, text="Scan all images in directory and save", command=save_dir,  width = '30', height = '4')
-
+button_screenshot = Button(window, text="Screenshot & OCR", command= lambda: screenshot(text1, text2), width = '30', height = '4')
 
 button_single.place(x=20, y=800)
 button_all.place(x=300, y=800)
+button_screenshot.place(x=600, y=800)
 
 window.mainloop()
